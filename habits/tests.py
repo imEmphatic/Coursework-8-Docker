f6747b7f600916e2bffc33529cebd8b79dd22d36
Aleksei Prusikov <imemphatic@yandex.ru>
2025-02-10 15:11:07 +0300

    edited files

--- habits/tests.py
+++ habits/tests.py
@@ -7,10 +7,7 @@ from users.models import User
 
 
 class HabitTestCase(APITestCase):
-    """Тесты для эндпоинтов модели Habit."""
-
     def setUp(self):
-        """Настройка тестовых данных для каждого теста."""
         self.user = User.objects.create(email="test@gmail.com")
         self.habit = Habit.objects.create(
             place="спортивная площадка",
@@ -19,7 +16,7 @@ class HabitTestCase(APITestCase):
             is_nice_habit=False,
             frequency_number=1,
             frequency_unit="days",
-            reward="съесть яблоко",
+            reward="съесть банан",
             duration="120",
             is_public=True,
             user=self.user,
@@ -27,7 +24,6 @@ class HabitTestCase(APITestCase):
         self.client.force_authenticate(user=self.user)
 
     def test_habit_retrieve(self):
-        """Тест получения информации о привычке."""
         url = reverse("habits:habits-detail", args=(self.habit.pk,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -35,12 +31,11 @@ class HabitTestCase(APITestCase):
         self.assertEqual(data.get("action"), self.habit.action)
 
     def test_habit_create(self):
-        """Тест создания новой привычки."""
         url = reverse("habits:habits-list")
         data = {
             "place": "дом",
             "time": "20:40:00",
-            "action": "убраться",
+            "action": "пропылесосить",
             "is_pleasant": False,
             "frequency_number": 1,
             "frequency_unit": "days",
@@ -54,13 +49,11 @@ class HabitTestCase(APITestCase):
         self.assertEqual(Habit.objects.all().count(), 2)
 
     def test_habit_list(self):
-        """Тест получения списка привычек."""
         url = reverse("habits:habits-list")
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_habit_update(self):
-        """Тест обновления данных привычки."""
         url = reverse("habits:habits-detail", args=(self.habit.pk,))
         data = {
             "reward": "посмотреть фильм",
@@ -70,13 +63,11 @@ class HabitTestCase(APITestCase):
         self.assertEqual(data.get("reward"), "посмотреть фильм")
 
     def test_public_habit_list(self):
-        """Тест получения списка публичных привычек."""
         url = reverse("habits:public")
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_habit_delete(self):
-        """Тест удаления привычки."""
         url = reverse("habits:habits-detail", args=(self.habit.pk,))
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
